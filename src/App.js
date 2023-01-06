f66872d8743c218aa7d03291a054cfcc5e43a796
sundarsp07 <113540684+sundarsp07@users.noreply.github.com>
2023-01-06 16:49:44 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import './App.scss';
+import { Header } from './layout'
+import { UserPage } from './pages'
+
+import { BrowserRouter, createBrowserRouter, RouterProvider } from "react-router-dom";
+
+function App() {
+
+
+  // const router = createBrowserRouter([
+  //   {
+  //     path: "/",
+  //     element: <Header/>,
+  //     // loader: rootLoader,
+  //     children: [
+  //       {
+  //         path: "team",
+  //         element:<UserPage/>,
+  //         // loader: teamLoader,
+  //       },
+  //     ],
+  //   },
+  // ]);
+
+
+  return (
+    <>
+      <Header />
+      <div className='container mt-5'>
+        <UserPage />
+      </div>
+
+    </>
+
+  );
+}
+
+export default App;
